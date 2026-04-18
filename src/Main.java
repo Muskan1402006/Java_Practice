d42cc45597273ecaba9cbab27e0fde9e8e7fceee
Muskan1402006 <muskanitkan3@gmail.com>
2026-04-18 22:19:42 +0530

    auto practice commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,17 @@ public class Main {
         int age=20;
         String neighbour ="varun";
         String friend= neighbour;
+
+        //byte- 1[-128 to 127]
+        //short -2
+        // int-4
+        //long-8
+        //float-4
+        //double-8
+        //char-2
+        //boolean- 1 true/false
+
+        
     }
 
 }
