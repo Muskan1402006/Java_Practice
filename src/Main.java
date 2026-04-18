4220746552c8696a49afbf3df4c1b428da7539bd
Muskan1402006 <muskanitkan3@gmail.com>
2026-04-18 22:09:39 +0530

    auto practice commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("hello muskan u are a lier");
-
+    //variables
+        String name="muskan";
+        int age=20;
+        String neighbour ="varun";
+        String friend= neighbour;
     }
 
 }
