58e692e45156ac10c5be695e039df69e0e1065f3
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-22 22:55:45 +0530

    variables

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello muskan ");
-    //variables
-        String name="muskan";
-        int age=20;
-        String neighbour ="Megha";
-        String friend= neighbour;
+        byte b=5;
+        short s=10;
+        int i=4000;
+        long l=10000;
 
-        //byte- 1[-128 to 127]
-        //short -2
-        // int-4
-        //long-8
-        //float-4
-        //double-8
-        //char-2
-        //boolean- 1 true/false
+        //Real numbers 
+        float f= 10.54f;
+        double d=23.0987;
 
-        byte agem =30;
-        int phone=12233;
-        long phone2=2121212343337675L;
-        float pi = 3.14F;
-        char letter= '@';
-        boolean isAdult= true;
+        System.out.print("Integer values ->" +b+ ","+s+","+i+","+l);
 
-//JUST FOR A CONTRIBUTION
-}}
+
+    }
+}
