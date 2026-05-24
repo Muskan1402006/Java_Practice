5eee23fa9101b9b5e63c0d8bd19b4e7f0ebcf197
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-24 22:28:24 +0530

    auto practice commit

--- auto.bat
+++ auto.bat
@@ -1,6 +1,15 @@
+@echo off
+
 :loop
 git add .
-git commit -m "auto practice commit"
-git push
+
+git diff --cached --quiet
+if %errorlevel%==0 (
+    echo No changes to commit
+) else (
+    git commit -m "auto practice commit"
+    git push
+)
+
 timeout /t 300
 goto loop
