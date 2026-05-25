6cf352ff307cc8555ba48c675604d5c13d12d29f
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-25 17:38:23 +0530

    loops

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-       while(i<=10){
-        System.out.println(i);
-        i++;
-       }
+          for(int i=1;i<=5;i++){
+            for(int j=1; j<=5; j++){
+                System.out.println(i);
+            }
+          }
 
         }
     }
