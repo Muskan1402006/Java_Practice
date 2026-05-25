c058dd11ebe1bee06b7a2b6f36e1103991b3af44
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-25 17:11:25 +0530

    loops

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 3;
-        switch (i) {
-            case 1:
-                System.out.println("i is 1");
-                break;
-
-            case 2:
-                System.out.println("i is 2");
-                break;
-            case 3:
-                System.out.println("i is 3");
-                break;
-            default:
-                System.out.println("i is nothing");
+        int i = 0;
+       while(i<=10){
+        System.out.println(i);
+        i++;
+       }
 
         }
     }
-}
