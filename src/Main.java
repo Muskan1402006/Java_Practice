e715de3ee71e388d1cb23670d3b0347886814f78
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-25 17:48:34 +0530

    continue

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
           for(int i=1;i<=5;i++){
-            for(int j=1; j<=5; j++){
-                System.out.print(i);
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
             }
+            System.out.println();
           }
 
         }
