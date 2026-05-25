d0aff99ebbc1832866f6ae3dd082bfee3f01cb3c
Muskan1402006 <muskanitkan3@gmail.com>
2026-05-25 17:42:57 +0530

    printing patterns

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
           for(int i=1;i<=5;i++){
             for(int j=1; j<=5; j++){
-                System.out.println(i);
+                System.out.print(i);
             }
           }
 
